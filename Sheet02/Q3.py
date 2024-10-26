b19d8738e9904bf17d0ec7dc1011459f709a4c7a
s68peiss <s68peiss@uni-bonn.de>
2024-10-26 15:53:24 +0200

    add 4 iamges

---
+++ Sheet02/Q3.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+img_path = 'einstein.jpeg'
+einstein_img = cv.imread(img_path)
+
+img_path = 'einstein_eye.jpeg'
+einstein_eye_img = cv.imread(img_path)
+
+def SSD(image):
+    pass
+
+def NCC(image):
+    pass
+
