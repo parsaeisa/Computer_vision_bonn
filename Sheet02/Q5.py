ca1d05440a77c219e54ded5a04c98b15137dfbfb
s68peiss <s68peiss@uni-bonn.de>
2024-10-26 16:12:05 +0200

    starting ...

--- Sheet02/Q5.py
+++ Sheet02/Q5.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 from helpers import display_image
+import matplotlib.pyplot as plt
 
 img_path = 'data/messi.jpg'
 messi_img = cv.imread(img_path)
@@ -8,3 +9,9 @@ img_path = 'data/ronaldo.jpeg'
 ronaldo_img = cv.imread(img_path)
 
 display_image("test", ronaldo_img)
+
+def build_gaussian_pyramid(image):
+    pass
+
+def build_laplacian_pyramid(image):
+    pass
