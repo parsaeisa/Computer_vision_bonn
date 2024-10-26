6e07b9334aa3a49f74013eb4232023e29198e9bd
s68peiss <s68peiss@uni-bonn.de>
2024-10-26 15:56:12 +0200

    add display_image helper method

---
+++ Sheet02/helpers.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+
+def display_image(window_name, img):
+    """
+    Displays image with given window name.
+    :param window_name: name of the window
+    :param img: image object to display
+    """
+    cv.imshow(window_name, img)
+    cv.waitKey(0)
+    cv.destroyAllWindows() 
